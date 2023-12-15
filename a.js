4ba20c35240a432f5ccce0fecc99234efbd00e75
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2023-12-15 08:57:58 +0100

    first commit

---
+++ a.js
@@ -0,0 +1,50 @@
+// learning javascript
+// const number = ['1','2','3','4','5','6']
+// console.log(number.length);
+// let you = []
+// console.log(you);
+// console.log(number[0])
+// console.log(number[3])
+// console.log(number[5])
+// const MixedDataType = [1,'true',0.98,null,undefined,'name'
+// ]
+// console.log(MixedDataType.length);
+// let companies = ['facebook','apple','IBM','oracle','google','microsoft','Amazon']
+// console.log(companies);
+// console.log(companies.length)
+// console.log(number[0])
+// console.log(companies[3])
+// console.log(companies[0])
+// let Uppercase = companies
+// console.log(Uppercase)
+// if( typeof companies == "object") {
+//     console.log('one of my companies')
+// }
+// else {
+//     console.log('company not found')
+// }
+// console.log(typeof companies);
+
+// for(let comp of companies) {
+//      if(comp.includes("oo")) {
+
+//         console.log(comp)
+//      }
+// }
+// companies.sort()
+// console.log(companies)
+// console.log(companies.reverse());
+// console.log(companies.slice(0,3))
+// console.log(companies.slice(4,7));
+// console.log(companies.splice(0,1))
+// console.log(companies.splice(3,1))
+// console.log(companies.splice(5,1))
+
+// // console.log(companies)
+// // console.log(companies.filter(0,4))
+// const letter = [1,2,3,4,5,6,7,8,9]
+// sum = 2
+// for(let i = 0; i < letter.length; i++){
+//     sum = sum + letter[i];
+//     console.log(sum);
+// }
